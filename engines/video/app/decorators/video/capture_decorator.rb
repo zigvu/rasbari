3e936591f36f608264b6a742b05f28fca2c15f13
Evan Acharya <eacharya@gmail.com>
2016-01-15 15:31:02 -0800

    Issue 2: Video: Flag captures that have not sent clips in last 10 minutes

--- engines/video/app/decorators/video/capture_decorator.rb
+++ engines/video/app/decorators/video/capture_decorator.rb
@@ -6,5 +6,18 @@ module Video
       object.clips.count == 0
     end
 
+    def hasEnded?
+      object.stopped_at != nil
+    end
+
+    def hasError?
+      lastClip = object.clips.last
+      # if the stream has not ended and no clip has been created in last 10 minutes
+      if !hasEnded? && lastClip
+        return ((Time.now - lastClip.created_at)/60 > 10)
+      end
+      false
+    end
+
   end
 end
--- engines/video/app/decorators/video/stream_decorator.rb
+++ engines/video/app/decorators/video/stream_decorator.rb
@@ -2,7 +2,16 @@ module Video
   class StreamDecorator < Draper::Decorator
     delegate_all
 
+    def hasError?
+      # if the stream is capturing but no clip has been created in last 10 minutes
+      if object.state.isCapturing? && lastClip
+        return ((Time.now - lastClip.created_at)/60 > 10)
+      end
+      false
+    end
+
     def lastClip
+      # if there is an ongoing capture, supply active clip, else stopped clip
       object.captures.where(stopped_at: nil).count > 0 ? activeLastClip : stoppedLastClip
     end
 
