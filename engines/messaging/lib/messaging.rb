9aa0cc43c91378ae5b2fe3e9d46580286aff5c54
Deepak Bajracharya <deepak@zigvu.com>
2017-05-20 23:24:23 +0000

    Issue-11: Logic to compare directory/files moved to messagin.rb instead; This also takes care of Issue-6 (module dependency issue) in nimki for storage server

--- engines/messaging/lib/messaging.rb
+++ engines/messaging/lib/messaging.rb
@@ -64,11 +64,40 @@ module Messaging
     # currently assume default URL
     Messaging::Connections::BunnyConnection.new(nil).connection
   end
+
+  # Compare directory first by depth
+  # then if required, by dictionary order
+  def self.dirnameComp(stra, strb)
+    a_count = countSlash( stra)
+    b_count = countSlash( strb)
+    ret = a_count <=> b_count
+    if ret == 0 
+      ret = stra <=> strb
+    end
+    ret
+  end
+
+  def self.countSlash(str) 
+    ret = 0
+    slashPos = 0;
+    if ! str.nil?
+      begin
+        slashPos = str.index('/', slashPos)
+        if !slashPos.nil?
+          ret += 1
+          slashPos += 1
+        end
+      end while !slashPos.nil?
+
+    end
+    ret
+  end
+
 end
 
 # load files if not used as engine
 if !Module.const_defined?('Rails')
-  Messaging.files_to_load.each { |file|
+  Messaging.files_to_load.sort {|a,b| Messaging.dirnameComp(a,b)} . each { |file|
     require_relative file
   }
 end
--- engines/messaging/lib/messaging/engine.rb
+++ engines/messaging/lib/messaging/engine.rb
@@ -17,32 +17,4 @@ module Messaging
     end
   end
 
-  # Compare directory first by depth
-  # then if required, by dictionary order
-  def self.dirnameComp(stra, strb)
-    a_count = countSlash( stra)
-    b_count = countSlash( strb)
-    ret = a_count <=> b_count
-    if ret == 0 
-      ret = stra <=> strb
-    end
-    ret
-  end
-
-  def self.countSlash(str) 
-    ret = 0
-    slashPos = 0;
-    if ! str.nil?
-      begin
-        slashPos = str.index('/', slashPos)
-        if !slashPos.nil?
-          ret += 1
-          slashPos += 1
-        end
-      end while !slashPos.nil?
-
-    end
-    ret
-  end
-
 end
