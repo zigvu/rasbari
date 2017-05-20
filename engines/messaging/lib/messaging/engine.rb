e9733d9334ab552f2fbd62ba5b9459b2fda9d9fd
Deepak Bajracharya <deepak@zigvu.com>
2017-05-20 22:36:06 +0000

    Issue-11: Rasbari Installation: Files loaded by depth of the directory and dictionrary order; Files at higher directory are loaded first; Required to prevent loading files from lower level directory, which are depended upon the higher level once, so that module dependency requirement are fulfilled during loading

--- engines/messaging/lib/messaging/engine.rb
+++ engines/messaging/lib/messaging/engine.rb
@@ -11,9 +11,38 @@ module Messaging
 
     # Load files after initialization
     initializer "messaging", after: :load_config_initializers do |app|
-      Messaging.files_to_load.each { |file|
+      Messaging.files_to_load.sort { |a, b| Messaging.dirnameComp(a, b) } .each { |file|
         require_relative file
       }
     end
   end
+
+  # Compare directory first by depth
+  # then if required, by dictionary order
+  def self.dirnameComp(stra, strb)
+    a_count = countSlash( stra)
+    b_count = countSlash( strb)
+    ret = a_count <=> b_count
+    if ret == 0 
+      ret = stra <=> strb
+    end
+    ret
+  end
+
+  def self.countSlash(str) 
+    ret = 0
+    slashPos = 0;
+    if ! str.nil?
+      begin
+        slashPos = str.index('/', slashPos)
+        if !slashPos.nil?
+          ret += 1
+          slashPos += 1
+        end
+      end while !slashPos.nil?
+
+    end
+    ret
+  end
+
 end
